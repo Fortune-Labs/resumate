cdf283ce099451e4261f0414310145fe3a0ea91f
Fortunatus <wulnyefortunatus@gmail.com>
2023-06-27 01:49:13 +0000

    components and browser router added

--- refrontend/src/App.jsx
+++ refrontend/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import React from "react";
 import './App.css'
+import Home from "./assets/home/home";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" name="Home" component={Home} />
+        <Route exact path="/login" name="Login Page" component={Login} />
+        {/* <Route exact path="/passwordReset" component={PasswordReset} /> */}
+        <Route
+          exact
+          path="/register"
+          name="Register Page"
+          component={Register}
+        />
+        <Route exact path="/initial" component={Initial} />
+        <Route exact path="/event-book" component={Eventbook} />
+        <Route exact path="/events" component={Events} />
+        <Route exact path="/booking" component={Booking} />
+        <Route exact path="/user" component={User} />
+      </Switch>
+    </BrowserRouter>
+  </div>
   )
 }
 
