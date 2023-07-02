787e618312bc49ccf0dda28a6f2802886eef9fed
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-02 05:58:20 +0000

    changes

--- refrontend/src/App.jsx
+++ refrontend/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import './App.css'
+import Navbar from './components/header/navbar'
+import Home from './pages/home/home'
+import Dashboard from './pages/dashboard/dashboard'
+import CV from './pages/cv/cv'
+import Resume from './pages/resume/resume'
+import Jobs from './pages/job/job'
+// import Education from './pages/education/education'
+// import Blog from './pages/blog/blog'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/dashboard' element={<Dashboard/>}/>
+            <Route exact path='/cv' element={<CV/>}/>
+            <Route exact path='/resume' element={<Resume/>}/>
+            <Route exact path='/jobs' element={<Jobs/>}/>
+            {/* <Route exact path='/education' element={<Education/>}/> */}
+            {/* <Route exact path='/blog' element={<Blog/>}/>  */}
+          </Routes>
+        <Footer/>
+      </Router>
+  
+    </div>
   )
 }
 
 export default App
+  
---
+++ refrontend/src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+
+const home = () => {
+  return (
+    <div className='main-wrapper'>
+      <h1>resumate homepage</h1>
+      <p>
+      Build a resume for the job you want. My Perfect Resume will help you every step of the way with tools, 
+      guides and expert advice. Try our Resume Builder 
+      and see for yourself — it’s fast, easy and backed by experts!
+      </p>
+    </div>
+  )
+}
+
+export default home
