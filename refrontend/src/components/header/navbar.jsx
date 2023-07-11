dc97955abc549f73c5bfd016954c3e420389f851
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-11 04:54:21 +0000

    changes

--- refrontend/src/components/header/navbar.jsx
+++ refrontend/src/components/header/navbar.jsx
@@ -1,155 +1,63 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import React, { useState } from "react";
+// import {logo,lock, hamburgerMenu, close} from '../assets'
 
-const navigation = [
-  { name: 'Home', href: '/', current: false },
-  { name: 'Dashboard', href: '/dashboard', current: false },
-  { name: 'CV', href: '/cv', current: false },
-  { name: 'Resume', href: '/resume', current: false },
-  { name: 'Job', href: '/job', current: false },
-  { name: 'Education', href: '/education', current: false },
-  { name: 'Blog', href: '/blog', current: false },
-]
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => setToggle(!toggle);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  {/* Company log */}
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="./svgs/logo.svg"
-                    alt="fortunelabs"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="./svgs/logo.svg"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
+    <div className="w-full h-[80px] bg-white border-b px-2 sm:px-6 lg:px-8">
+      <div className="md:max-w-[1480px] max-w-[600px] m-auto w-full h-full flex justify-between items-center md:px-0 px-4">
+        <img src="./svgs/logo.svg" className="h-[25px]" />
+        <div className="hidden md:flex items-center ">
+          <ul className="flex gap-4">
+            <li> <a href="/">Home</a></li>
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="resume">Resume</a></li>
+            <li><a href="/jon">Job</a></li>
+            <li><a href="/education">Education</a></li>
+            <li><a href="/blog">Blog</a></li>
+          </ul>
+        </div>
+        <div className="hidden md:flex">
+          <button className="flex justify-between items-center  bg-transparent  px-6 gap-2">
+            <img src="./svgs/lock.svg" />
+            Login
+          </button>
+          <button className="px-8 py-3 rounded-md bg-[#20B486] text-white font-bold">
+            Sign Up For Free
+          </button>
+        </div>
+        <div className="md:hidden" onClick={handleClick}>
+          <img src="./svgs/hamburgerMenu.svg" />
+        </div>
+      </div>
+      <div
+        className={
+          toggle
+            ? "absolute z-10 p-4  bg-white w-full px-8 md:hidden border-b"
+            : "hidden"
+        }
+      >
+        <ul>
+          <li className="p-4 hover:bg-gray-100 ">Home</li>
+          <li className="p-4 hover:bg-gray-100">About</li>
+          <li className="p-4 hover:bg-gray-100">Support</li>
+          <li className="p-4 hover:bg-gray-100">Platform</li>
+          <li className="p-4 hover:bg-gray-100">Pricing</li>
+          <div className="flex flex-col my-4 gap-4">
+            <button className="border border-[20B486] flex justify-center items-center  bg-transparent  px-6 gap-2 py-4">
+              <img src="./svgs/lock.svg" />
+              Login
+            </button>
+            <button className="px-8 py-5 rounded-md bg-[#20B486] text-white font-bold">
+              Sign Up For Free
+            </button>
           </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  )
-}
+export default Navbar;
--- refrontend/src/pages/home/home.jsx
+++ refrontend/src/pages/home/home.jsx
@@ -1,26 +1,105 @@
+import React from 'react'
+import Card from './card'
+import Slider from "react-slick";
+import { templates } from '../../data/Templates';
 
 const home = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  };
+  
   return (
-    <div className='main-wrapper'>
-      <div>
-      <h1>resumate homepage</h1>
-      <p>
-      Build a resume for the job you want. My Perfect Resume will help you every step of the way with tools, 
-      guides and expert advice. Try our Resume Builder 
-      and see for yourself — it’s fast, easy and backed by experts!
-      
-      </p>
-      </div>
-      <div>
-        Sign Up and Login
-      </div>
-      <div>
-       More details
-       Examples
-      </div>
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      {/* Hero Section */}
+      <section className='w-full bg-white py-24'>
+        <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 max-w-[600px]  px-4 md:px-0'>
+            <div className='flex flex-col justify-start gap-4'>
+                <p className='py-2 text-2xl text-[#20B486] font-medium'>RESUMATE TO SUCCESS</p>
+                <h1 className='md:leading-[72px] py-2 md:text-6xl text-5xl font-semibold'>Create your Resume 
+                    from <span  className='text-[#20B486]'>100+</span> Templates
+                    & Institutions
+                </h1>
+                <p className='py-2 text-lg text-gray-600'>Build a resume for the job you want.
+                  Resumate will help you every step of the way with tools, guides and expert advice. 
+                  Try our Resume Builder for free - it’s fast, easy and backed by experts!.
+                </p>
+                <button>
+                    Start
+                </button>
+            </div>       
+            <img  src= "./images/resumesample.png"className="md:order-last  order-first" />
+        </div>
+      </section>
+      {/* How to Section */}
+      <section className='w-full bg-white py-24'>
+        <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
+            <h1 className='text-center text-2xl font-bold text-[#536E96]'>Build your resume online in 4 simple steps.</h1>
+            <p className='text-center  text-[#536E96] text-xl'>Making a resume with our resume maker is fast and easy. We'll help you build a professional job application in minutes.</p>
+            <div className='flex justify-center py-8 md:gap-8 '>
+                <div className='grid md:grid-cols-4 grid-cols-2 gap-2'>
+                  <img src="./svgs/logo.svg" />
+                  <img src="./svgs/logo.svg"/>
+                  <img src="./svgs/logo.svg" />
+                  <img src="./svgs/logo.svg" />
+                </div>
+            </div>
+        </div>
+      </section>
 
+      {/* Templates */}
+      <section className='w-full bg-[#E9F8F3B2] py-32'>
+        <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
+            <div className='py-4'>
+              <h1 className='py-3 text-3xl font-bold'>Most Popular <span className='text-[#20B486]'>Templates</span></h1>
+              <p className='text-[#6D737A]'>Various versions have evolved over the years, sometimes by accident.</p>
+            </div>
+            
+            <Slider {...settings} className='px-5'>
+              {templates.map((template,i)=>
+                <div key={i}>
+                  <Card template={template} />
+                </div> ) }
+            </Slider>     
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default home
