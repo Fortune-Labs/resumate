ebdd1c65f5d2ce52faffefe0165fa902be5e6a3a
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-12 00:05:00 +0000

    Login created

---
+++ refrontend/src/pages/accregister/register.jsx
@@ -0,0 +1,20 @@
+const register = () => {
+    return (
+      <div>
+        <h1 className="text-center p-4">Register</h1>
+        <p className="mx-5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quaerat in
+           minima facere cumque consectetur velit unde rem officiis necessitatibus non digniss
+           imos laborum quo quidem adipisci, ut quisquam error ad!
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quaerat in
+           minima facere cumque consectetur velit unde rem officiis necessitatibus non digniss
+           imos laborum quo quidem adipisci, ut quisquam error ad!
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quaerat in
+           minima facere cumque consectetur velit unde rem officiis necessitatibus non digniss
+           imos laborum quo quidem adipisci, ut quisquam error ad!
+        </p>
+      </div>
+    )
+  }
+  
+  export default register
