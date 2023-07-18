210d016f8560ddddd691992184ba8518fa8115ba
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-18 18:47:52 +0000

    nav bar buttons colored

--- refrontend/src/components/header/navbar.jsx
+++ refrontend/src/components/header/navbar.jsx
@@ -24,46 +24,23 @@ const Navbar = () => {
         </div>
         <div className="hidden md:flex gap-3">
     {/* Button for sign up */}
-        <Button variant="outlined" className="flex items-center gap-3">
+        <Button variant="outlined" className="flex items-center bg-gray-300 gap-3">
         <a href="/login">Login</a> 
       </Button>
      {/* Button for sign up */}
-      <Button  variant="outlined" className="flex items-center gap-3 px-8 py-3">
+      <Button  variant="outlined" className="flex items-center bg-gray-300 gap-3 px-8 py-3">
         <a href="/register">Sign Up</a> 
       </Button>
-      <Button  variant="outlined" className="flex items-center gap-3 px-8 py-3">
+      <Button  variant="outlined" className="flex items-center  bg-gray-300 gap-3 px-8 py-3">
         <a href="/dashboard">My Account</a> 
       </Button>
 
-        </div>
+      </div>
         <div className="md:hidden" onClick={handleClick}>
           <img src="./svgs/hamburgerMenu.svg" />
         </div>
       </div>
-      <div
-        className={
-          toggle
-            ? "absolute z-10 p-4  bg-white w-full px-8 md:hidden border-b"
-            : "hidden"
-        }
-      >
-        <ul>
-          <li className="p-4 hover:bg-gray-100 ">Home</li>
-          <li className="p-4 hover:bg-gray-100">About</li>
-          <li className="p-4 hover:bg-gray-100">Support</li>
-          <li className="p-4 hover:bg-gray-100">Platform</li>
-          <li className="p-4 hover:bg-gray-100">Pricing</li>
-          <div className="flex flex-col my-4 gap-4">
-            <button className="border border-[20B486] flex justify-center items-center  bg-transparent  px-6 gap-2 py-4">
-              <img src="./svgs/lock.svg" />
-              Login
-            </button>
-            <button className="px-8 py-5 rounded-md bg-[#20B486] text-white font-bold">
-              Sign Up For Free
-            </button>
-          </div>
-        </ul>
-      </div>
+      
     </div>
   );
 };
